617cc4982e4d0d974e8a05a36e4883989ff07f70
Janowie <krcmar.jan@gmail.com>
2021-02-04 23:35:50 +0100

    stage 1 - ready for final touch ups

---
+++ visualize/src/components/canvas/canvases.js
@@ -0,0 +1,225 @@
+import React from "react";
+import Mario from "../../static/mario_bg.png";
+import {dot} from "mathjs";
+
+export default function Canvases(props) {
+
+  const canvas = React.useRef(null);
+  const canvas_output = React.useRef(null);
+
+  const [state, setState] = React.useState({
+    "ctx": null,
+    "ctx_output": null,
+  })
+
+  // Initialize state
+  React.useEffect(() => {
+    if (canvas && canvas_output) {
+      props.state.image.crossOrigin = 'anonymous';
+      props.state.image.src = Mario;
+      props.state.image.onload = () => {
+
+        initCanvases();
+
+        setState({
+          ...state,
+          "ctx": canvas.current.getContext("2d"),
+          "ctx_output": canvas_output.current.getContext("2d")
+        });
+
+        // console.log(canvas.current.height, canvas.current.width);
+        // props.setCanvasDims(640,640);
+      }
+    }
+  }, [canvas, canvas_output])
+
+  React.useEffect(() => {
+    if (state.ctx) {
+      drawKernel(0, 0);
+    }
+  }, [state.ctx])
+
+  React.useEffect(() => {
+    if (
+      props.state.interval == null && props.state.pixel_size > 0
+    ) {
+      initCanvases();
+    }
+  }, [props.state.pixel_size, props.state.background_color])
+
+  React.useEffect(() => {
+    if (props.tick_counter) {
+      draw();
+    }
+  })
+
+  // ###########################################################################################
+  // ## METHODS
+  // ###########################################################################################
+
+  const drawLine = (ctx, f_x, f_y, t_x, t_y) => {
+    ctx.beginPath();
+    ctx.moveTo(f_x, f_y);
+    ctx.lineTo(t_x, t_y);
+    ctx.strokeStyle = 'rgba(182,182,182,0.5)';
+    if (props.state.pixel_size < 3)
+      ctx.strokeStyle = 'rgba(182,182,182,0.01)';
+    ctx.stroke();
+  }
+
+  const drawPixels = (canvas) => {
+    let ctx = canvas.getContext("2d");
+    for (let row = 0; row < canvas.height; row += props.state.pixel_size) {
+      drawLine(ctx, 0, row, canvas.width, row);
+      drawLine(ctx, row, 0, row, canvas.height);
+    }
+  }
+
+  const drawBackgroundImage = (canvas) => {
+
+    let ctx = canvas.getContext("2d");
+    let offset = props.state.pixel_size;
+
+    // Draw background
+    ctx.fillStyle = props.state.background_color;
+    ctx.fillRect(0, 0, 1000, 1000);
+
+    // Draw image
+    ctx.drawImage(props.state.image, offset, offset, canvas.height-offset*2, canvas.width-offset*2);
+    drawPixels(canvas);
+
+  }
+
+  const initCanvases = () => {
+    drawBackgroundImage(canvas.current);
+    let ctx_output = canvas_output.current.getContext('2d');
+    // Draw background
+    ctx_output.fillStyle = props.state.background_color;
+    ctx_output.fillRect(0, 0, 1000, 1000);
+    drawPixels(canvas_output.current);
+  }
+
+  const getPixel = (x, y) => {
+    if (state.ctx) {
+      let pixel = state.ctx.getImageData(x, y, 1, 1);
+      let data = pixel.data;
+
+      return data;
+    }
+    return [];
+  }
+
+  const drawOutputPixel = (x, y, color) => {
+    if (state.ctx_output) {
+      state.ctx_output.fillStyle = color;
+      state.ctx_output.fillRect(x, y, props.state.pixel_size, props.state.pixel_size);
+    }
+  }
+
+  const split_channels = (values) => {
+    let r = [];
+    let g = [];
+    let b = [];
+    let a = [];
+
+    for (let i = 0; i < values.length; i++) {
+      r.push(values[i][0]);
+      g.push(values[i][1]);
+      b.push(values[i][2]);
+      a.push(values[i][3]);
+    }
+
+    return [r, g, b, a];
+  }
+
+  const filter_conv = (x, y, values) => {
+
+    let kernel = props.kernel;
+    let v = split_channels(values);
+
+    let color = `rgba(${dot(v[0], kernel)},${dot(v[1], kernel)},${dot(v[2], kernel)},255)`;
+    // let color = `rgba(${v[0][4]},${v[1][4]},${v[2][4]},255)`;
+    // console.log("color", color);
+
+    return color;
+  }
+
+  const drawKernel = (x, y) => {
+
+    let border_width = 1;
+    let size = props.state.kernel_size * props.state.pixel_size - border_width;
+    let dot_size = 2;
+
+    state.ctx.fillStyle = "rgba(44, 62, 80,0.5)";
+    state.ctx.fillRect(x, y, size, size);
+    state.ctx.strokeStyle = "rgba(241, 196, 15,1.0)";
+    state.ctx.strokeRect(x, y, size, size);
+
+    for (let j = 0; j < props.state.kernel_size; j++) {
+      for (let i = 0; i < props.state.kernel_size; i++) {
+        if (i === 1 && j === 1) {
+          dot_size = 6
+        }
+        else {
+          dot_size = 2
+        }
+          state.ctx.fillRect((x + props.state.pixel_size / 2) + props.state.pixel_size * i - dot_size/2, (y + props.state.pixel_size / 2) + props.state.pixel_size * j - dot_size/2, dot_size, dot_size);
+      }
+    }
+  }
+
+  const getKernelValues = (x, y) => {
+    let values = [];
+    for (let j = 0; j < props.state.kernel_size; j++) {
+      for (let i = 0; i < props.state.kernel_size; i++) {
+        values.push(getPixel((x + props.state.pixel_size / 2) + props.state.pixel_size * i, (y + props.state.pixel_size / 2) + props.state.pixel_size * j))
+      }
+    }
+    return values;
+  }
+
+  const draw = () => {
+
+    if (!state.ctx) {
+      return null;
+    }
+
+    let x = props.tick_counter.x * props.state.pixel_size;
+    let y = props.tick_counter.y * props.state.pixel_size;
+
+    // Original canvas
+    drawBackgroundImage(canvas.current);
+    let values = getKernelValues(x, y);
+    drawKernel(x, y);
+
+    // Output canvas
+    drawOutputPixel(x + props.state.pixel_size, y + props.state.pixel_size, filter_conv(x, y, values));
+
+  }
+
+  return (
+    <div className="row mb-5">
+      <div className="col-lg-6 col-md-12 col-sm-12">
+        <h3> Original </h3>
+        <canvas
+          ref={canvas}
+          width="640"
+          height="640"
+        >
+          Your browser does not support the HTML5 canvas tag.
+        </canvas>
+      </div>
+      <div className="col-lg-6 col-md-12 col-sm-12">
+        <h3> Filtered </h3>
+        <canvas
+          ref={canvas_output}
+          width="640"
+          height="640"
+        >
+          Your browser does not support the HTML5 canvas tag.
+        </canvas>
+      </div>
+    </div>
+
+  )
+}
--- visualize/src/components/canvas/kernel.js
+++ visualize/src/components/canvas/kernel.js
@@ -1,248 +1,174 @@
 import React from "react";
-import PragueImage from "../../static/mario.png";
+import Canvases from "./canvases";
+import KernelSettings from "./kernels";
 
-const drawMario = (canvas) => {
-
-  var ctx = canvas.getContext("2d");
-
-  var marioArray=[
-    // [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,2,2,2,2,2,2,0,0,0,0,0,0],
-    [0,0,0,2,2,2,2,2,2,2,2,2,2,0,0,0],
-    [0,0,0,3,3,3,1,1,1,0,1,0,0,0,0,0],
-    [0,0,3,1,3,1,1,1,1,1,1,1,1,0,0,0],
-    [0,0,3,1,3,3,1,1,1,1,3,1,1,1,0,0],
-    [0,0,3,3,1,1,1,1,1,3,3,3,3,0,0,0],
-    [0,0,0,1,1,1,1,1,1,1,1,1,0,0,0,0],
-    [0,0,2,2,2,4,2,2,4,2,0,0,0,0,0,0],
-    [0,2,2,2,2,4,2,2,4,2,2,2,0,0,0,0],
-    [2,2,2,2,2,4,4,4,4,2,2,2,2,0,0,0],
-    [1,1,1,2,4,1,4,4,1,4,2,1,1,0,0,0],
-    [1,1,1,1,4,4,4,4,4,4,1,1,1,0,0,0],
-    [1,1,1,4,4,4,4,4,4,4,4,1,1,0,0,0],
-    [0,0,0,4,4,4,4,4,4,4,4,0,0,0,0,0],
-    [0,0,4,4,4,4,4,0,4,4,4,4,0,0,0,0],
-    [0,4,4,4,4,4,4,0,4,4,4,4,4,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    // [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-
-  ]
-  //starting position
-  let offset = 32;
-  var xPos = 0 + offset;
-  var yPos = 0 + offset;
-
-  for(var i=0; i < marioArray.length; i++){
-
-    for(var r=0; r < marioArray[i].length; r++ ){
-      ctx.fillRect(xPos, yPos, 32, 32);
-      //black
-     if(marioArray[i][r]===0){
-       ctx.fillStyle = "#000000";
-     }
-     //flesh
-      if(marioArray[i][r]===1){
-       ctx.fillStyle = "#FFCC66";
-     }
-     //red
-      if(marioArray[i][r]===2){
-       ctx.fillStyle = "#FF0000";
-     }
-     //brown
-     if(marioArray[i][r]===3){
-       ctx.fillStyle = "#663300";
-     }
-     //blue
-     if(marioArray[i][r]===4){
-       ctx.fillStyle = "#66CCFF";
-     }
-    //move over 32px
-    xPos+=32
-    }//end internal for loop
-    //once ctx reaches end on canvas reset xPos to 0
-    xPos=0 + offset;
-    //move down 32px
-    yPos+=32;
-  }//end for loop
-
-  return ctx.getImageData(0, 0, 400, 400);
-
-}
 
 export default function Kernel(props) {
-  const canvas = React.useRef(null);
-  const canvas_output = React.useRef(null);
 
-  let tick_counter = {
-    "x": 0,
-    "y": 0
-  }
+  let tick_counter = null;
 
   const [state, setState] = React.useState({
-    "ctx": null,
-    "ctx_output": null,
-    "canvas_size": 0,
     "interval": null,
     "kernel_size": 3,
-    "pixel_size": 32,
-    "fps": 500,
+    "kernel": [ // Initialized with a gaussian blur
+        1/16, 1/8, 1/16,
+        1/8, 1/4, 1/8,
+        1/16, 1/8, 1/16
+    ],
+    "pixel_size": 18,
+    "fps": 100,
+    "image": new Image(),
+    "background_color": "#e74c3c",
+    "canvas_height": 640,
+    "canvas_width": 640,
+    "tick_counter": {
+        "x": 0,
+        "y": 0
+      }
   })
 
-  // Initialize state
-  React.useEffect(() => {
-    if (canvas && canvas_output) {
-      drawMario(canvas.current);
-      setState({
-        ...state,
-        "ctx": canvas.current.getContext("2d"),
-        "ctx_output": canvas_output.current.getContext("2d")
-      });
-    }
-  }, [canvas, canvas_output])
-
-
-  // ###########################################################################################
-  // ## METHODS
-  // ###########################################################################################
-
   const tick = () => {
-    let num_ticks = (canvas.current.width * canvas.current.height) / (state.pixel_size * state.pixel_size)
-    if (tick_counter <= num_ticks) {
-      tick_counter += 1;
-      draw(tick_counter);
-    }
-  }
 
-  const pickColor = (x, y) => {
-    if (state.ctx) {
-      let pixel = state.ctx.getImageData(x, y, 1, 1);
-      let data = pixel.data;
-
-      // const rgba = `rgba(${data[0]}, ${data[1]}, ${data[2]}, ${data[3] / 255})`;
-      // return rgba;
-
-      return data;
-    }
-    return [];
-  }
-
-  const drawOutputPixel = (x, y, color) => {
-    if (state.ctx_output) {
-      state.ctx_output.fillRect(x, y, state.pixel_size, state.pixel_size);
-      state.ctx_output.fillStyle = color;
-    }
-  }
-
-  const filter_threshold_ticked = (x, y) => {
-    let threshold = 100
-
-    let pixelColor = pickColor(x, y);
-
-    // console.log("filter pixel color", pixelColor, pixelColor[0], pixelColor[1], pixelColor[2], pixelColor[3]);
-
-    // let v = (0.2126*r + 0.7152*g + 0.0722*b >= threshold) ? 255 : 0;
-    if (pixelColor.length) {
-      return `rgba(${pixelColor[0]}, ${pixelColor[1]}, ${pixelColor[2]}, ${pixelColor[3]})`;
-    }
-    else
-      return "rgb(0,0,0)"
-  }
-
-  const getCoords = tick => {
-    let x = ((tick -1)  * state.pixel_size) % canvas.current.width;
-
-    let ticks_per_row = Math.floor(canvas.current.width / state.pixel_size);
-    let row = Math.trunc( (tick-1) / ticks_per_row);
-
-    // Make sure the filter does not go away from the picture
-    if (tick % (ticks_per_row - state.kernel_size) === 0) {
-      x = 0;
-      row += 1;
-    }
-
-    let y = row * state.pixel_size;
-
-    return {"x": x, "y": y}
-  }
-
-  const draw = (tick) => {
-
-    if (!state.ctx) {
-      return null;
-    }
-
-    drawMario(canvas.current);
-
-    let size = state.kernel_size * state.pixel_size - 1;
-    const {x, y} = getCoords(tick);
-
-    // Mriezka
-
-    for (let j = 0; j < state.kernel_size; j++) {
-      for (let i = 0; i < state.kernel_size; i++) {
-        state.ctx.strokeRect((x + state.pixel_size / 2) + state.pixel_size * i, (y + state.pixel_size / 2) + state.pixel_size * j, 1, 1);
+    if (!tick_counter) {
+      tick_counter = {
+        "x": 0,
+        "y": 0
       }
     }
 
-    drawOutputPixel(x, y, filter_threshold_ticked(x, y));
+    let ticks_per_row = Math.floor(state.canvas_width / state.pixel_size) - Math.floor(state.kernel_size/2) + 2 - state.kernel_size;
+    let num_rows = Math.floor(state.canvas_height / state.pixel_size) + 2 - state.kernel_size;
 
-    state.ctx.strokeStyle = "#ff0000";
-    state.ctx.strokeRect(x, y, size, size);
+    // Stop at the end
+    if (tick_counter.y == num_rows - 1 && tick_counter.x == ticks_per_row) {
+      stopTick();
+      return null;
+    }
+
+    // Update position
+    if (tick_counter.x < ticks_per_row) {
+      tick_counter.x += 1;
+    }
+    else {
+      tick_counter.x = 0;
+      // Set y
+      if (tick_counter.y < num_rows) {
+        tick_counter.y += 1;
+      }
+    }
+
+    setState({
+      ...state,
+      "tick_counter": tick_counter
+    })
+
+  }
+
+  const stopTick = () => {
+    // Stop animation - set tick_counter
+    clearInterval(state.interval);
+    setState({
+      ...state,
+      "interval": null,
+      "tick_counter": tick_counter
+    })
   }
 
   const toggleAnimation = () => {
     if (state.interval == null) {
       // Reset tick counter
-      tick_counter = state.tick_counter || 0;
-      let interval = setInterval(() => tick(), state.fps);
+      if (state.tick_counter)
+        tick_counter = state.tick_counter;
+      let interval = setInterval(tick, state.fps);
       setState({
         ...state,
         "interval": interval
       })
     }
-    else {
-      // Stop animation - set tick_counter
-      clearInterval(state.interval);
-      setState({
-        ...state,
-        "interval": null,
-        "tick_counter": tick_counter
-      })
-    }
+    else
+      stopTick();
   }
 
   const handleClick = () => {
-    if (canvas.current.width && canvas.current.height) {
+    if (state.canvas_width && state.canvas_height) {
       toggleAnimation();
     }
   }
 
+  const handleChange = (key, value) => {
+    setState({
+      ...state,
+      [key]: value
+    })
+  }
 
   return (
-    <div className="mario-canvas">
+    <section className="container mario-canvas">
 
-      <button className="btn btn-primary" onClick={handleClick}>Start / stop animation</button>
+      <h1 className="mt-3 mb-4">Filter visualization</h1>
 
-      <div className="row">
-        <div className="col-lg-6 col-md-12 col-sm-12">
-          <h3> Original </h3>
-          <canvas
-            ref={canvas}
-            width="576"
-            height="640"
+      <article className="controls mb-5">
+
+        <h3>Options</h3>
+
+        <div className="form-group pt-3">
+          <label htmlFor="background_color" className="form-label">Background color: </label>
+          <input type="color" className="form-control" id="background_color"
+                 value={state.background_color}
+                 onChange={e => handleChange(e.target.id, e.target.value)}
           />
         </div>
-        <div className="col-lg-6 col-md-12 col-sm-12">
-          <h3> Filtered </h3>
-          <canvas
-            ref={canvas_output}
-            width="576"
-            height="640"
+
+        <div className="form-group pt-3">
+          <label htmlFor="pixel_size" className="form-label">Pixel size: </label>
+          <input type="number" className="form-control" id="pixel_size"
+                 value={state.pixel_size}
+                 onChange={e =>
+                   handleChange(e.target.id, parseInt(e.target.value))
+                 }
           />
         </div>
-      </div>
 
-    </div>
+        <div className="form-group pt-3">
+          <label htmlFor="speed" className="form-label">Speed: </label>
+
+          <select
+            value={state.fps}
+            onChange={e => handleChange("fps", parseInt(e.target.value))}
+            className="form-control"
+            id="speed"
+          >
+            <option value="500">Slow</option>
+            <option value="100">Normal</option>
+            <option value="16">Fast</option>
+            <option value="0">Ultra fast</option>
+          </select>
+
+        </div>
+
+        <KernelSettings
+          key="kernel_settings"
+          kernel_size={state.kernel_size}
+          kernel={state.kernel}
+          setKernel={(val) => handleChange("kernel", val)}
+        />
+
+
+        <button className="btn btn-primary mt-3" onClick={handleClick}>Start / stop animation</button>
+      </article>
+
+      <Canvases
+        key="canvases"
+        state={state}
+        kernel={state.kernel}
+        tick_counter={state.tick_counter}
+        // setCanvasDims={(h, w) => {
+        //   handleChange("canvas_height", 640);
+        //   handleChange("canvas_width", 640);
+        // }}
+      />
+
+
+    </section>
   )
 }
